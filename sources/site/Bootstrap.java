260fa6a80567041a977cb994650a10d769340089
suhiang <suhiang@yahoo.com.sg>
2014-12-03 11:12:23 +0800

    Update Bootstrap.java

--- sources/site/Bootstrap.java
+++ sources/site/Bootstrap.java
@@ -7,7 +7,7 @@ import javax.servlet.http.*;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.*;
 
-class Bootstrap {
+class Boot strap {
     public static void main(String[] args) throws Exception {
         Server server = new Server(getPort());
         server.setHandler(new ResourceHandler() {
